e759289ea63b85d3cf4e54dfe8e40527dbb72daf
VIPUZB5555 <ogabekor5555@gmail.com>
2025-05-31 13:23:17 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+
+# .env fayldan token o‘qish
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -47,6 +53,30 @@ def calculator():
 @app.route('/services')
 def services():
     return render_template('services.html')
+
+@app.route('/ai-chat', methods=['POST'])
+def ai_chat():
+    data = request.get_json()
+    user_msg = data.get("message", "")
+    
+    headers = {
+        "Authorization": f"Bearer {os.getenv('HF_TOKEN')}"
+    }
+    payload = {
+        "inputs": user_msg
+    }
+
+    response = requests.post(
+        "https://api-inference.huggingface.co/models/google/flan-t5-base",
+        headers=headers,
+        json=payload
+    )
+    try:
+        reply = response.json()[0]["generated_text"]
+    except:
+        reply = "Kechirasiz, javob bera olmadim."
+    
+    return jsonify({"reply": reply})
 
 if __name__ == '__main__':
     app.run(debug=True)
